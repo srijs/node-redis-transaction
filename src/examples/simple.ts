be044ab51ad6fec526e3c171549a93bc4e57bf73
Sam Rijs <srijs@airpost.net>
2016-03-21 23:31:49 +1100

    extend example

--- src/examples/simple.ts
+++ src/examples/simple.ts
@@ -16,7 +16,24 @@ const t = new tx.Transaction(async (ctx) => {
   console.log(get1, get2, get3);
 });
 
+const k2 = new tx.Key('bar', tx.Kind.List);
+const s2 = tx.Scope.fromKeys([k2]);
+
+const t2 = new tx.Transaction(async (ctx) => {
+  const exists1 = await ctx.withKey(k2).exists();
+  const get1 = await ctx.withListAt(k2).length();
+  await ctx.withListAt(k2).append(['bar']);
+  const exists2 = await ctx.withKey(k2).exists();
+  const get2 = await ctx.withListAt(k2).length();
+  await ctx.withListAt(k2).append(['baz']);
+  const exists3 = await ctx.withKey(k2).exists();
+  const get3 = await ctx.withListAt(k2).length();
+  console.log(exists1, exists2, exists3);
+  console.log(get1, get2, get3);
+});
+
 const p = new tx.redis.Pool({});
 const c = new tx.redis.Redis(p);
 
 t.exec(s, c);
+t2.exec(s2, c);
